615593c82203e9e5062411aeed01ab15fa741428
Thibaud Av <thibaud.avenier@gmail.com>
2014-05-09 20:45:04 +0200

    ouvir et ajouter une experience

--- app.js
+++ app.js
@@ -10,22 +10,27 @@ window.ondragover = window.ondrop = function(e) {e.preventDefault();return false
 var express = require('express')
   , routes = require('./routes');
 
-var gui = global.window.nwDispatcher.requireNwGui();// Load native UI library
-var win = gui.Window.get();// Get the current window
 
+
+
+var gui = global.window.nwDispatcher.requireNwGui();// Load native UI library
+
+var win = gui.Window.get();// Get the current window
+    win.show();
 var app = module.exports = express.createServer();
 
 // Configuration
 
-win.setMinimumSize(800, 549);
+// win.setMinimumSize(800, 549);
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
+  app.set('view options', {layout: __dirname + '/views/layout.ejs'});
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
-  app.use(express.session({ secret: 'secretEmoLyse' }));
+  app.use(express.session({ secret: 'secret' }));
   app.use(app.router);
   app.use('/public',express.static(__dirname + '/public'));
 });
@@ -38,26 +43,38 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
+
+
+
 // Routes
+app.get('/', function(req, res) {
 
-app.get('/', routes.index);
+    res.redirect('/index');
+});
 
-// Nouvelle experience 
-app.post('/nouvExperience', function(req, res) {
-    if (req.body.nomExp != '') {
-        res.redirect('/expEtape1');
-    }
-    req.session.error = "Pas de nom de projets.";
-    res.redirect('/');
-})
+app.get('/index', routes.index);
+
+
+// POST Nouvelle Experience
+app.post('/newExperience', routes.newExperience);
+
+// POST Ouvrir Experience
+app.post('/openExperience', routes.openExperience);
+
+app.get('/saveExperience', routes.saveExperience);
+
+// POST Nouvel Participant ( Participent )
+app.post('/newParticipant', routes.newParticipant);
+// POST Nouvel Evaluation
+app.post('/newEvaluation', routes.newEvaluation);
 
 app.get('/expEtape1', routes.expEtape1);
 app.get('/expEtape2', routes.expEtape2);
-app.get('/expEtape3', routes.expEtape3);
+app.get('/evaluations', routes.evaluations);
 
 
-app.listen(3001, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.listen(3002, function(){
+  // console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,151 @@
  * GET home page.
  */
 
+var _EmoLyse = require('./../lib/EmoLyse');
+
+var fs = require("fs");
+var EmoLyse = new _EmoLyse();
+
+
 exports.index = function(req, res){
-	error = '';
+  // On requpére l'error si il y en a.
 	error = req.session.error;
+  // On supprimer l'error car on la réqupéré
 	req.session.error = null;
-  	
-  	res.render('index', { 
-  		title: 'Emolyse - Home',
-  		error: error,
+
+  // On recupére la liste des configs
+  _configs = EmoLyse.getConfigs();
+  // On recupére la lise des experiences
+  _experiences = EmoLyse.getExperiences();
+
+  res.render('index', { 
+  	title: 'Emolyse',
+    configs: _configs,
+    experiences: _experiences,
+    showMenuExperience:false,
+  	error: error,
  	})
 };
 
+
+
+exports.newExperience = function(req, res) {
+
+    if (req.body.nomExp != '') {
+      if (req.body.configExp != '' &&  typeof req.body.configExp != 'undefined') {
+      
+        EmoLyse.newExperience(req.body.nomExp,req.body.descriptionExp,req.body.configExp,req.body.imageExp);
+
+        res.redirect('/evaluations');
+      } else req.session.error = "Il faut choisir une configuration";
+    } else req.session.error = "Le nom de l'eperience est vide";
+
+    res.redirect('/');
+};
+
+exports.openExperience = function(req, res) {
+
+      if (req.body.experience != '' &&  typeof req.body.experience != 'undefined') {
+      
+        EmoLyse.openExperience(req.body.experience);
+
+        res.redirect('/evaluations');
+      } else req.session.error = "Il faut choisir une experience";
+
+    res.redirect('/');
+};
+
+
+
+exports.saveExperience = function(req, res) {
+    var util = require('util');
+  res.send(util.inspect(EmoLyse.saveExperience(), false, null));
+};
+
+exports.newParticipant = function(req, res) {
+
+    if (req.body.numUser != '') {
+      if (req.body.sexeUser == 'H'|| req.body.sexeUser == 'F') {
+
+        if (req.body.dateNaissance != '') {
+
+          if (req.body.lvlEtude != '' &&  typeof req.body.lvlEtude != 'undefined') {
+      
+            // EmoLyse.newExperience(req.body.nomExp,req.body.descriptionExp,req.body.configExp);
+            EmoLyse.experience.addParticipant(req.body.numUser,req.body.sexeUser,req.body.dateNaissance,req.body.lvlEtude);
+            res.redirect('/evaluations');
+
+          } else req.session.error = "Il faut choisir un niveau d'etude";
+        } else req.session.error = "Il faut remplire la date de naissanace";
+      } else req.session.error = "Il faut indiquer le sexe du participant";
+    } else req.session.error = "Il faut indiquer le numéro du participant";
+
+    res.redirect('/evaluations');
+};
+
+
+exports.newEvaluation = function(req, res) {
+
+    if (req.body.numUser != '') {
+      if (req.body.sexeUser == 'H'|| req.body.sexeUser == 'F') {
+
+        if (req.body.dateNaissance != '') {
+
+          if (req.body.lvlEtude != '' &&  typeof req.body.lvlEtude != 'undefined') {
+      
+            // EmoLyse.newExperience(req.body.nomExp,req.body.descriptionExp,req.body.configExp);
+            res.redirect('/evaluations');
+
+          } else req.session.error = "Il faut choisir un niveau d'etude";
+        } else req.session.error = "Il faut remplire la date de naissanace";
+      } else req.session.error = "Il faut indiquer le sexe du participant";
+    } else req.session.error = "Il faut indiquer le numéro du participant";
+
+    res.redirect('/');
+};
+
+exports.evaluations = function(req, res){
+  // On requpére l'error si il y en a.
+  error = req.session.error;
+  // On supprimer l'error car on la réqupéré
+  req.session.error = null;
+
+  res.render('evaluations', { 
+    title: 'Emolyse - Evaluations',
+    showMenuExperience:true,
+    error: error,
+    participants:EmoLyse.experience.participants,
+  })
+};
+
+
 exports.expEtape1 = function(req, res){
+
+    var util = require('util');
+
+  // On requpére l'error si il y en a.
+  error = req.session.error;
+  // On supprimer l'error car on la réqupéré
+  req.session.error = null;
+
+
   res.render('expEtape1', { 
-  	title: 'Emolyse - Experience',
+    title: 'Emolyse - Experience',
+    showMenuExperience:true,
+    error: error,
+    config: util.inspect(EmoLyse.experience.configuration.emotions, false, null),
   })
 };
 
 exports.expEtape2 = function(req, res){
-  res.render('expEtape2', { 
-  	title: 'Emolyse - Experience',
-  })
-};
+  // On requpére l'error si il y en a.
+  error = req.session.error;
+  // On supprimer l'error car on la réqupéré
+  req.session.error = null;
 
-exports.expEtape3 = function(req, res){
-  res.render('expEtape3', { 
-    title: 'Emolyse - Experience',
+  res.render('expEtape2', { 
+    title: 'Emolyse - Experience'+EmoLyse.experience.nom,
+    showMenuExperience:true,
+    error: error,
   })
 };
